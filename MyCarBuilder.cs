841aea94c7ba5af154a69b963f571436789cadab
Elina Prykhodko <eprykhodko@mint-innovations.com>
2024-08-30 16:28:16 +0300

    Initial commit

---
+++ MyCarBuilder.cs
@@ -0,0 +1,43 @@
+﻿using System.Text;
+
+namespace BuilderPattern
+{
+    public class TiresCar
+    {
+        public string TypeTiresCar { get; set; }
+    }
+
+    public class EngineCar
+    {
+        public string TypeEngineCar { get; set; }
+    }
+
+    public class BodyCar
+    {
+        public string TypeBodyCar { get; set; }
+    }
+
+    public class GPSCar { }
+
+    public class Car
+    {
+        public TiresCar TiresCar { get; set; }
+        public EngineCar EngineCar { get; set; }
+        public BodyCar BodyCar { get; set; }
+        public GPSCar GPSCar { get; set; }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            if (TiresCar != null)
+                sb.Append("Шини: " + TiresCar.TypeTiresCar + "\n");
+            if (EngineCar != null)
+                sb.Append("Двигун: " + EngineCar.TypeEngineCar + "\n");
+            if (BodyCar != null)
+                sb.Append("Кузов: " + BodyCar.TypeBodyCar + "\n");
+            if (GPSCar != null)
+                sb.Append("GPS: Наявний" + '\n');
+            return sb.ToString();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,92 @@
+﻿using BuilderPattern;
+using System;
+
+    static void Main(string[] args)
+    {
+    CarMechanikBuilder mechanik = new CarMechanikBuilder();
+
+    SportCarBuild sportCarBuilder = new SportCarBuild();
+    Car sportCar = mechanik.DrawUp(sportCarBuilder);
+    Console.WriteLine("Спортивний автомобіль:");
+    Console.WriteLine(sportCar.ToString());
+
+
+    SUVBuilder suvbuilder = new SUVBuilder();
+    Car suvCar = mechanik.DrawUp(suvbuilder);
+    Console.WriteLine("Позашляховик:");
+    Console.WriteLine(suvCar.ToString());
+
+
+}
+public abstract class CarBuild
+{
+    public Car Car { get; private set; }
+    public void CreateCar()
+    {
+        Car = new Car();
+    }
+    public abstract void SetTiresCar();
+    public abstract void SetTypeEngineCar();
+    public abstract void SetTypeBodyCar();
+    public abstract void SetGPSCar();
+}
+
+public class CarMechanikBuilder
+{
+    public Car DrawUp(CarBuild carBuild)
+    {
+        carBuild.CreateCar();
+        carBuild.SetTiresCar();
+        carBuild.SetTypeEngineCar();
+        carBuild.SetTypeBodyCar();
+        carBuild.SetGPSCar();
+
+        return carBuild.Car;
+    }
+}
+
+public class SportCarBuild : CarBuild
+{
+    public override void SetTiresCar()
+    {
+        this.Car.TiresCar = new TiresCar { TypeTiresCar = "Літні шини" };
+    }
+
+    public override void SetTypeEngineCar()
+    {
+        this.Car.EngineCar = new EngineCar { TypeEngineCar = "V8" };
+    }
+
+    public override void SetTypeBodyCar()
+    {
+        this.Car.BodyCar = new BodyCar { TypeBodyCar = "Купе" };
+    }
+
+    public override void SetGPSCar()
+    {
+        this.Car.GPSCar = new GPSCar();
+    }
+}
+
+public class SUVBuilder : CarBuild
+{
+    public override void SetTiresCar()
+    {
+        this.Car.TiresCar = new TiresCar { TypeTiresCar = "Зимові шини" };
+    }
+
+    public override void SetTypeEngineCar()
+    {
+        this.Car.EngineCar = new EngineCar { TypeEngineCar = "Q5" };
+    }
+
+    public override void SetTypeBodyCar()
+    {
+        this.Car.BodyCar = new BodyCar { TypeBodyCar = "Позашляховик" };
+    }
+
+    public override void SetGPSCar()
+    {
+        this.Car.GPSCar = new GPSCar();
+    }
+}
